dcbd8bbbed2fd9771f81db3f905907fb53fa63b4
H2OLooBots <ftcteam7247@gmail.com>
2024-01-29 17:22:35 -0600

    idk what I did

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,12 +52,12 @@ public final class Constants {
         public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
         // SPARK MAX CAN IDs
-        public static final int kFrontLeftDrivingCanId = 11;
+        public static final int kFrontLeftDrivingCanId = 1;
         public static final int kRearLeftDrivingCanId = 13;
         public static final int kFrontRightDrivingCanId = 15;
         public static final int kRearRightDrivingCanId = 17;
 
-        public static final int kFrontLeftTurningCanId = 10;
+        public static final int kFrontLeftTurningCanId = 2;
         public static final int kRearLeftTurningCanId = 12;
         public static final int kFrontRightTurningCanId = 14;
         public static final int kRearRightTurningCanId = 16;
---
+++ src/main/java/frc/robot/SingleSwerveModule.java
@@ -0,0 +1,74 @@
+package frc.robot;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.SwerveModule;
+
+public class SingleSwerveModule extends TimedRobot {
+    SwerveModule module = new SwerveModule(1, 2, 10, 0);
+
+    @Override
+    public void robotInit() {
+        module.resetEncoders();
+    }
+
+    /**
+     * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+     * that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+    }
+
+    /**
+     * This function is called once each time the robot enters Disabled mode.
+     */
+    @Override
+    public void disabledInit() {
+    }
+
+    @Override
+    public void disabledPeriodic() {
+    }
+
+    /**
+     * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+     */
+    @Override
+    public void autonomousInit() {
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void teleopInit() {
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
+        module.setDesiredState(new SwerveModuleState());
+    }
+
+    @Override
+    public void testInit() {
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
+}
