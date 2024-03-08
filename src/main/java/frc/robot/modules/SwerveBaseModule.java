5f1fb7392e73c711060cb612bed9414e8ccc5251
NicholasRHeinz <55408394+NicholasRHeinz@users.noreply.github.com>
2024-03-07 20:59:39 -0600

    Swerve Code
    Bulk of the code written, not implemented into the rest of code. Still want to add some test functions to make diagnostics easier

---
+++ src/main/java/frc/robot/modules/SwerveBaseModule.java
@@ -0,0 +1,117 @@
+package frc.robot.modules;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SwerveBaseModule {
+    public SwerveModule[] modules;
+    public SwerveModulePosition[] positions;
+
+    public static final double kWheelOffset = Units.inchesToMeters(13.5);
+    public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(new Translation2d[] {
+            new Translation2d(kWheelOffset, kWheelOffset),
+            new Translation2d(-kWheelOffset, kWheelOffset),
+            new Translation2d(kWheelOffset, -kWheelOffset),
+            new Translation2d(-kWheelOffset, -kWheelOffset)
+    });
+
+    public SwerveDriveOdometry odometry;
+
+    DriveBaseStates current_state;
+
+    XboxController input_controller;
+
+    public SwerveBaseModule(XboxController drive_controller) {
+        /* Create the four swerve modules passing in each corner's CAN ID */
+        this.modules = new SwerveModule[] {
+                /* Front Left */
+                new SwerveModule(2, 3, 4),
+                /* Front Right */
+                new SwerveModule(5, 6, 7),
+                /* Rear Left */
+                new SwerveModule(8, 9, 10),
+                /* Rear Right */
+                new SwerveModule(11, 12, 13)
+        };
+
+        this.positions = new SwerveModulePosition[4];
+        positions[0] = modules[0].get_module_position();
+        positions[1] = modules[1].get_module_position();
+        positions[2] = modules[2].get_module_position();
+        positions[3] = modules[3].get_module_position();
+
+        this.odometry = new SwerveDriveOdometry(kinematics, null, positions);
+    }
+
+    private void drive_xbox() {
+        /* Get the inputs from the controller */
+        double x = input_controller.getLeftX();
+        double y = input_controller.getLeftY();
+        double rotation = input_controller.getRightX();
+
+        /* Apply a deadband to prevent stick drift */
+        x = MathUtil.applyDeadband(x, 0.1);
+        y = MathUtil.applyDeadband(y, 0.1);
+        rotation = MathUtil.applyDeadband(rotation, 0.1);
+
+        /* Multiply each by max velocity to get desired velocity in each direction */
+        double x_velocity_m_s = x * Units.feetToMeters(16);
+        double y_velocity_m_s = y * Units.feetToMeters(16);
+        double rotational_vel = rotation * 4;
+
+        /*
+         * Convert velocity in each axis to a general chassis velocity then use the
+         * kinematics to convert the chassic velocity to individual swerve modules
+         * "states" ie rotation and drive velocity.
+         */
+        ChassisSpeeds speeds = new ChassisSpeeds(x_velocity_m_s, y_velocity_m_s, rotational_vel);
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speeds);
+
+        /* Send the new states to each swerve module */
+        setModuleStates(states);
+    }
+
+    private void lock() {
+        SwerveModuleState[] states = {
+                new SwerveModuleState(0, Rotation2d.fromDegrees(-45)),
+                new SwerveModuleState(0, Rotation2d.fromDegrees(45)),
+                new SwerveModuleState(0, Rotation2d.fromDegrees(45)),
+                new SwerveModuleState(0, Rotation2d.fromDegrees(-45))
+        };
+        setModuleStates(states);
+    }
+
+    public void update() {
+        switch (current_state) {
+            case XBOX:
+                drive_xbox();
+                break;
+            case LOCK:
+                lock();
+                break;
+            default:
+                lock();
+        }
+
+    }
+
+    public void setModuleStates(SwerveModuleState[] states) {
+        for (int i = 0; i < 4; i++) {
+            modules[i].set_module_state(states[i]);
+        }
+    }
+
+    public enum DriveBaseStates {
+        XBOX,
+        LOCK
+    }
+}
--- src/main/java/frc/robot/modules/SwerveModule.java
+++ src/main/java/frc/robot/modules/SwerveModule.java
@@ -3,36 +3,64 @@ package frc.robot.modules;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveModule {
     /* Motors */
-    CANSparkMax drive_motor;
-    CANSparkMax steer_motor;
+    CANSparkMax drive_spark;
+    CANSparkMax steer_spark;
 
     /* Encoders */
-    CANcoder angle_enc;
-    RelativeEncoder drive_enc;
+    CANcoder steer_cancoder;
+    RelativeEncoder drive_encoder;
 
     /* PID Controllers */
+    PIDController drive_controller;
     PIDController angle_controller;
 
-    public SwerveModule(CANSparkMax drive, CANSparkMax steer, CANcoder angle_m )
-    {
-        drive_motor = drive;
-        drive_enc = drive.getEncoder();
-        steer_motor = steer;
-        angle_enc = angle_m;
+    private Rotation2d last_angle;
+
+    public SwerveModule(int steer_id, int drive_id, int angle_id) {
+        drive_spark = new CANSparkMax(drive_id, MotorType.kBrushless);
+        drive_encoder = drive_spark.getEncoder();
+        steer_spark = new CANSparkMax(steer_id, MotorType.kBrushless);
+        steer_cancoder = new CANcoder(angle_id);
 
         angle_controller = new PIDController(0.006, 0, 0);
-        angle_controller.enableContinuousInput(0, 180);
-    } 
-
-    public double get_raw_angle()
-    {
-        return angle_enc.getAbsolutePosition().getValue() * 360; 
+        angle_controller.enableContinuousInput(0, 360);
     }
 
-    
+    public void set_module_state(SwerveModuleState state) {
+        /*
+         * Optimize the state - this handles reversing direction and minimizes the
+         * change in heading
+         */
+        state = SwerveModuleState.optimize(state, last_angle);
+
+        double drive_output = drive_controller.calculate(drive_encoder.getVelocity(), state.speedMetersPerSecond);
+        double steer_output = angle_controller.calculate(get_raw_angle(), state.angle.getDegrees());
+
+        /* Set the new powers to the SPARK Max controllers */
+        drive_spark.set(drive_output);
+        steer_spark.set(steer_output);
+
+        /* Update last angle for use next time */
+        last_angle = state.angle;
+
+    }
+
+    public double get_raw_angle() {
+        return steer_cancoder.getAbsolutePosition().getValue() * 360;
+    }
+
+    public SwerveModulePosition get_module_position() {
+        return new SwerveModulePosition(drive_encoder.getPosition(),
+                Rotation2d.fromDegrees(this.get_raw_angle()));
+    }
+
 }
